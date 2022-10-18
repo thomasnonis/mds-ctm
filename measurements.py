Added embedding and extracting in HL and LH sides of the dwt. TODO: Fix threshold calculation, add in parameters set of original images or retrieve it with the name

measurements.py
@@ -1,4 +1,3 @@
-from tkinter import image_names
 import numpy as np
 import os
 from scipy.signal import convolve2d
@@ -13,8 +12,9 @@ from tools import *
 from config import *
 
 def wpsnr(img1: np.ndarray, img2: np.ndarray):
-	if not os.path.isfile('csf.csv'):  
+	if not os.path.isfile('csf.csv'):
 		os.system('python -m wget "https://drive.google.com/uc?export=download&id=1w43k1BTfrWm6X0rqAOQhrbX6JDhIKTRW" -o csf.csv')
+		print("Ok")
 
 	img1 = np.float32(img1)/255.0
 	img2 = np.float32(img2)/255.0
@@ -135,12 +135,13 @@ def compute_thr_multiple_images(images, original_watermark, show: bool = True):
 	print('Total number of computations: %d' % n_computations)
 
 	# step by step for clarity
-	for original_img, watermarked_img, img_name in images:
+	for watermarked_img, img_name in images:
+		print(original_img, " ", watermarked_img)
 		(_, alpha, svd_key) = read_parameters(img_name)
 
 		for j in range(0, RUNS_PER_IMAGE):
 			attacks_list = get_random_attacks(randint(1, MAX_N_ATTACKS))
-			attacked_img, attacks_list = do_random_attacks(watermarked_img,attacks_list)
+			attacked_img, attacks_list = do_random_attacks(watermarked_img, attacks_list)
 
 			extracted_watermark = extract_watermark(original_img, img_name, attacked_img)
 