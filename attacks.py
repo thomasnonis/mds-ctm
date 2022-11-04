b728d9d11daf7fa37cc9cc033962b787ab32f440
IvanValentini <34308333+IvanValentini@users.noreply.github.com>
2022-11-04 10:59:46 +0100

    Allow to do attacks with specific parameters whilst hiding complexity

--- attacks.py
+++ attacks.py
@@ -21,15 +21,76 @@ from config import *
 def gaussian_blur(img, sigma):
 	return gaussian_filter(img, sigma)
 
+def wrapper_gaussian_blur(sigma = -1, min_sigma = 1, max_sigma = 15):
+	'''
+	Gaussian Blur Attack wrapper, returns an attack to be used in do_attacks
+	'''
+	if sigma == -1:
+		sigma = round(randint(min_sigma, max_sigma) * 2 / 10, 1) # 0.2, 0.4, ..., 3.0
+	return {
+		'function' : gaussian_blur,
+		'arguments' : {
+			"sigma" : sigma
+		},
+		'description' : 'Gaussian Blur ({})'.format(sigma)
+	}
+
 def average_blur(img, kernel):
 	return cv2.blur(img, (kernel,kernel))
+
+def wrapper_average_blur(avg_blur_kernel_size = -1, min_avg_blur_kernel_size = 1, max_avg_blur_kernel_size = 3):
+	'''
+	Average Blur Attack wrapper, returns an attack to be used in do_attacks
+	'''
+	if avg_blur_kernel_size == -1:
+		avg_blur_kernel_size = (randint(min_avg_blur_kernel_size, max_avg_blur_kernel_size) * 2) + 1 # 3, 5, 7
+	return {
+		'function' : average_blur,
+		'arguments' : {
+			"kernel" : avg_blur_kernel_size
+		},
+		'description' : 'Average Blur ({})'.format(avg_blur_kernel_size)
+	}
 
 def sharpen(img, sigma, alpha):
 	blurred = gaussian_filter(img, sigma)
 	return img + alpha * (img - blurred)
 
+def wrapper_sharpen(sigma = -1, alpha = -1, min_sigma = 0.2, max_sigma = 5, min_alpha = 0.1, max_alpha = 5):
+	'''
+	Sharpen Attack wrapper, returns an attack to be used in do_attacks
+	'''
+	# TODO: fix ranges
+	if sigma == -1:
+		sigma = round((random() * (max_sigma - min_sigma)) + min_sigma, 2)	
+	if alpha == -1:
+		alpha = round(random() * (max_alpha - min_alpha) + min_alpha, 2)
+	return {
+		'function' : sharpen,
+		'arguments' : {
+			"sigma" : sigma,
+			"alpha": alpha
+		},
+		'description' : 'Sharpen ({}, {})'.format(sigma, alpha)
+	}
+
 def median(img, kernel_size):
 	return medfilt2d(img, kernel_size)
+
+def wrapper_median(kernel_size = -1, min_kernel_size = 1, max_kernel_size = 3):
+	'''
+	Median Attack wrapper, returns an attack to be used in do_attacks
+	'''
+	if kernel_size == -1:
+		kernel_size = randint(min_kernel_size, max_kernel_size) * 2 + 1 # 3, 5, 7 (5 and 7 are not valid standalone, but may become valid after a sharpen)
+	
+	return {
+		'function' : median,
+		'arguments' : {
+			"kernel_size" : kernel_size
+		},
+		'description' : 'Median ({})'.format(kernel_size)
+	}
 
 def resizing(img, scale):
   x, y = img.shape
@@ -41,12 +102,44 @@ def resizing(img, scale):
 
   return attacked
 
+
+def wrapper_resizing(resize_scale = -1, min_resize_scale = 1, max_resize_scale = 9):
+	'''
+	Resizing Attack wrapper, returns an attack to be used in do_attacks
+	'''
+	if resize_scale == -1:
+		resize_scale = round(randint(min_resize_scale, max_resize_scale) / 10, 1) # 0.1, 0.2, ..., 0.9
+	return {
+		'function' : resizing,
+		'arguments' : {
+			"scale" : resize_scale
+		},
+		'description' : 'Resize ({})'.format(resize_scale)
+	}
+
 def awgn(img, std, seed):
 	mean = 0.0
 	np.random.seed(seed)
 	attacked = img + np.random.normal(mean, std, img.shape)
 	attacked = np.clip(attacked, 0, 255)
 	return attacked
+
+
+def wrapper_awgn(awgn_std_dev = -1, min_std_dev = 1, max_std_dev = 10):
+	'''
+	AWGN Attack wrapper, returns an attack to be used in do_attacks
+	'''
+	if awgn_std_dev == -1:
+		awgn_std_dev = randint(min_std_dev, max_std_dev) * 5 # 5, 10, 15, 20, 25, 30, 35, 40, 45
+	awgn_seed = randint(0, 1000)
+	return {
+		'function' : awgn,
+		'arguments' : { 
+			"std":  awgn_std_dev,
+			"seed": awgn_seed
+		},
+		'description' : 'Additive White Gaussian Noise ({}, {})'.format(awgn_std_dev, awgn_seed)
+	}
 
 
 def jpeg_compression(img, QF):
@@ -61,201 +154,59 @@ def jpeg_compression(img, QF):
 			print(f"Error while trying to remove {filename}") # We'll return the original img
 	return attacked
 
+def wrapper_jpeg_compression(quality_factor = -1, min_quality_factor = 1, max_quality_factor = 99):
+	'''
+	JPEG Compression Attack wrapper, returns an attack to be used in do_attacks
+	'''
+	if quality_factor == -1:
+		quality_factor = randint(min_quality_factor, max_quality_factor)
+	return {
+		'function' : jpeg_compression,
+		'arguments' : {
+			"QF" : quality_factor
+		},
+		'description' : 'JPEG ({})'.format(quality_factor)
+	}
+
 def get_random_attacks(num_attacks):
+	'''
+	Returns a list containing num_attacks random Attacks with random parameters. 
+	This list is to be used with do_attacks.
+	'''
 	attacks_list = []
 
 	for _ in range(0, num_attacks):
 		attack = randint(0, N_AVAILABLE_ATTACKS - 1)
 
 		if attack == 0:
-			awgn_std_dev = randint(1, 10) * 5 # 5, 10, 15, 20, 25, 30, 35, 40, 45
-			awgn_seed = randint(0, 1000)
-			attacks_list.append(
-				{
-					'function' : awgn,
-					'arguments' : { 
-						"std":  awgn_std_dev,
-						"seed": awgn_seed
-					},
-					'description' : 'Additive White Gaussian Noise ({}, {})'.format(awgn_std_dev, awgn_seed)
-				}
-			)
-		elif attack == 1:
-			avg_blur_kernel_size = (randint(1, 3) * 2) + 1 # 3, 5, 7
-			attacks_list.append(
-				{
-					'function' : average_blur,
-					'arguments' : {
-						"kernel" : avg_blur_kernel_size
-					},
-					'description' : 'Average Blur ({})'.format(avg_blur_kernel_size)
-				}
-			)
+			attacks_list.append(wrapper_awgn())
+		elif attack == 1:			
+			attacks_list.append(wrapper_average_blur())
 		elif attack == 2:
-			# TODO: fix ranges
-			sharpen_sigma = round((random() * (5 - 0.2)) + 0.2, 2)
-			sharpen_alpha = round(random() * (5 - 0.1) + 0.1, 2)
-
-			attacks_list.append(
-				{
-					'function' : sharpen,
-					'arguments' : {
-						"sigma" : sharpen_sigma,
-						"alpha": sharpen_alpha
-					},
-					'description' : 'Sharpen ({}, {})'.format(sharpen_sigma, sharpen_alpha)
-				}
-			)
+			attacks_list.append(wrapper_sharpen())
 		elif attack == 3:
-			qf = [1, 2, 3, 4, 5, 7, 9, 11, 13, 16, 19, 22, 25, 30, 35, 40, 50, 60, 65, 70, 75, 80, 82, 84, 86, 88, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100] # distribution optimized for wpsnr impact
-			jpeg_quality_factor = qf[randint(0, 36)] # len(qf) = 37
-			attacks_list.append(
-				{
-					'function' : jpeg_compression,
-					'arguments' : {
-						"QF" : jpeg_quality_factor
-					},
-					'description' : 'JPEG ({})'.format(jpeg_quality_factor)
-				}
-			)
-		elif attack == 4:
-			resize_scale = round(randint(1, 9) / 10, 1) # 0.1, 0.2, ..., 0.9
-			attacks_list.append(
-				{
-					'function' : resizing,
-					'arguments' : {
-						"scale" : resize_scale
-					},
-					'description' : 'Resize ({})'.format(resize_scale)
-				}
-			)
+			attacks_list.append(wrapper_jpeg_compression())
+		elif attack == 4:			
+			attacks_list.append(wrapper_resizing())
 		elif attack == 5:
-			median_kernel_size = randint(1, 3) * 2 + 1 # 3, 5, 7 (5 and 7 are not valid standalone, but may become valid after a sharpen)
-			attacks_list.append(
-				{
-					'function' : median,
-					'arguments' : {
-						"kernel_size" : median_kernel_size
-					},
-					'description' : 'Median ({})'.format(median_kernel_size)
-				}
-			)
-		elif attack == 6:
-			sigma = round(randint(1, 15) * 2 / 10, 1) # 0.2, 0.4, ..., 3.0
-			attacks_list.append(
-				{
-					'function' : gaussian_blur,
-					'arguments' : {
-						"sigma" : sigma
-					},
-					'description' : 'Gaussian Blur ({})'.format(sigma)
-				}
-			)
+			attacks_list.append(wrapper_median())
+		elif attack == 6:			
+			attacks_list.append(wrapper_gaussian_blur())
 		else:
 			exit('Invalid attack %d, check that N_AVAILABLE_ATTACKS is correct' % attack)
 	return attacks_list
 
 def describe_attacks(attacks_list):
+	'''
+	Returns a description for the attacks in an attacks list
+	'''
 	return ", ".join([attacks['description'] for attacks in attacks_list])
 
 def do_attacks(img, attacks_list):
+	'''
+	Execute a list of attacks on an image one after the other sequentially
+	'''
 	for attack in attacks_list:
 		attack['arguments']['img'] = img
 		img = attack['function'](**attack['arguments'])
 	return img, describe_attacks(attacks_list)
-
-def get_attacks_list(attacks):
-	attacks_list_out = []
-	for attack in attacks:
-		if attack == "gaus blur":
-			sigma = [randint(1, 5), randint(1, 5)]
-			attacks_list_out.append(
-				{
-					'function': gaussian_blur,
-					'arguments': {
-						"sigma": sigma
-					},
-					'description': 'Gaussian Blur ({})'.format(sigma)
-				}
-			)
-
-		if attack == "avg blur":
-			avg_blur_kernel_size = (randint(1, 3) * 2) + 1  # 3, 5, 7
-			attacks_list_out.append(
-				{
-					'function': average_blur,
-					'arguments': {
-						"kernel": avg_blur_kernel_size
-					},
-					'description': 'Average Blur ({})'.format(avg_blur_kernel_size)
-				}
-			)
-
-		elif attack == "jpeg":
-			jpeg_quality_factor = randint(1, 10) * 10  # 10, 20, ..., 100
-			attacks_list_out.append(
-				{
-					'function': jpeg_compression,
-					'arguments': {
-						"QF": jpeg_quality_factor
-					},
-					'description': 'JPEG ({})'.format(jpeg_quality_factor)
-				}
-			)
-
-		elif attack == "sharpen":
-			sharpen_sigma = (random() * (5 - 0.2)) + 0.2
-			sharpen_alpha = random() * (5 - 0.1) + 0.1
-
-			attacks_list_out.append(
-				{
-					'function': sharpen,
-					'arguments': {
-						"sigma": sharpen_sigma,
-						"alpha": sharpen_alpha
-					},
-					'description': 'Sharpen ({}, {})'.format(sharpen_sigma, sharpen_alpha)
-				}
-			)
-
-		elif attack == "awgn":
-			awgn_mean = randint(-5, 5)
-			awgn_std_dev = (random() * (5 - 0.2)) + 0.2
-			awgn_seed = randint(0, 1000)
-			attacks_list_out.append(
-				{
-					'function': awgn,
-					'arguments': {
-						"mean": awgn_mean,
-						"std": awgn_std_dev,
-						"seed": awgn_seed
-					},
-					'description': 'Additive White Gaussian Noise ({}, {}, {})'.format(awgn_mean, awgn_std_dev,
-																					   awgn_seed)
-				}
-			)
-
-		elif attack == "resize":
-			resize_scale = randint(1, 9) / 10  # 0.1, 0.2, ..., 0.9
-			attacks_list_out.append(
-				{
-					'function': resize,
-					'arguments': {
-						"scale": resize_scale
-					},
-					'description': 'Resize ({})'.format(resize_scale)
-				}
-			)
-
-		elif attack == "median":
-			median_kernel_size = (randint(1, 3) * 2) + 1  # 3, 5, 7
-			attacks_list_out.append(
-				{
-					'function': median,
-					'arguments': {
-						"kernel_size": median_kernel_size
-					},
-					'description': 'Median ({})'.format(median_kernel_size)
-				}
-			)
-	return attacks_list_out
